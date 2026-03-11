a60b50f3aa5ab894d8d37a8d35166bc3801f4b8d
nicolas97alonso <nicolas97alonso@gmail.com>
2026-03-11 17:10:49 +0100

    Add files via upload

--- dsa/linked_list/exercises.py
+++ dsa/linked_list/exercises.py
@@ -1,7 +1,6 @@
-from ll import LinkedList, Node
+from .ll import LinkedList, Node
 
 
-# 1. Find middle node
 def find_middle_node(ll: LinkedList) -> Node:
     slow = ll.head
     fast = ll.head
@@ -9,78 +8,43 @@ def find_middle_node(ll: LinkedList) -> Node:
     while fast and fast.next:
         slow = slow.next
         fast = fast.next.next
-
     return slow
 
 
-# 2. Has loop
-
-
-def has_loop(ll: LinkedList) -> bool:
+def has_loop(ll: LinkedList) -> Node:
     slow = ll.head
     fast = ll.head
 
     while fast and fast.next:
         slow = slow.next
         fast = fast.next.next
-        if slow == fast:
+        if fast == slow:
             return True
-
     return False
 
 
-# 3. Find kth node from end
-
-
-def find_k_end_node(ll: LinkedList, i: int) -> Node:
-    slow = ll.head
-    fast = ll.head
-
-    for _ in range(i):
-        fast = fast.next
-        if not fast:
-            return None
-
-    while fast:
-        slow = slow.next
-        fast = fast.next
-
-    return slow
-
-
-# 4. Remove duplicates
-
+# TODO: Investigate unnit test
 
 if __name__ == "__main__":
-
     ll = LinkedList()
-    ll.append(3)
-    ll.append(4)
-    ll.append(6)
     ll.append(2)
-    ll.append(1)
-
+    ll.append(3)
+    ll.append(2)
     ll.print_list()
-    print("--------")
-    print("Check middle node")
+    print("----------")
+    print("find_middle_node_test")
     print(find_middle_node(ll).value)
-    print("--------")
-    print("Check has loop false")
-    print(has_loop(ll))
-    print("--------")
+    print("----------")
+    ll.append(4)
+    ll.print_list()
+    print("----------")
+    print("find_middle_node_test")
+    print(find_middle_node(ll).value)
+    print("----------")
+    print("has_loop_test")
     ll2 = LinkedList()
+    ll2.append(2)
     ll2.append(3)
     ll2.append(4)
-    ll2.append(6)
-    ll2.append(2)
-    ll2.append(1)
-    ll2.tail.next = ll2.head
-    print("Check k end node i = 1, 1")
-    print(find_k_end_node(ll, 1).value)
-    print("--------")
-    print("Check k end node i = 2, 2")
-    print(find_k_end_node(ll, 2).value)
-    print("--------")
-    print("Check k end node i = 3, 6")
-    print(find_k_end_node(ll, 3).value)
-    print("--------")
+    ll2.tail = ll2.head
+    print(has_loop(ll2))
