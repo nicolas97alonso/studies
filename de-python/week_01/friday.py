c52918d1ee02964ad0f5ba15588283ad8f5f9d21
nicolas97alonso <nicolas97alonso@gmail.com>
2026-03-26 12:29:24 +0100

    Add files via upload

---
+++ de-python/week_01/friday.py
@@ -0,0 +1,14 @@
+# FRIDAY — Make it a CLI tool
+# ----------------------------
+# Goal: Make the script runnable from the terminal
+#
+# Instructions:
+# 1. Bring together everything from Wed + Thu (both functions)
+# 2. Use sys.argv to accept the filepath as a command line argument
+# 3. Use the if __name__ == "__main__": pattern to run everything
+# 4. It should work like this from the terminal:
+#    python friday.py run_results.json
+#
+# ✅ Check: it runs from the terminal without touching the code
+
+# Your code below:
